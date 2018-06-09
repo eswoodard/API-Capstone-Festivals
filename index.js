ebb733d3c05e327d794973bcaa0425ab07c16637
Elizabeth Woodard <eswoodard75@gmail.com>
2018-06-09 14:49:12 -0400

    event listener added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+//main page loads
+//user enters zipcode and clicks submit
+//event listener listens for submit 
+//query sent to API
+//API responds with data object
+//festival events displayed in list
+//festival markers displayed on map
+//user clicks on event
+//light box opens and displays event details and new map
+//user clicks on map marker
+//light box opens and displays event details and new map
+//user clicks on "x" or outside of lightbox
+//lightbox closes
 
 
 const EVENTBRITE_SEARCH_URL = "https://www.eventbriteapi.com/v3/events/search/";
@@ -15,7 +28,7 @@ function getDataFromEventbrite(zipcode){
 
 function handleResponse(response){
 	console.log(response);
-	const eventListHTML = getEventListHTML(response);
+	const eventListHTML = renderEventListHTML(response);
 	renderEventList(eventListHTML);
 }
 
@@ -23,11 +36,11 @@ function handleResponse(response){
 $.ajax(settings).done(handleResponse);
 
 
-function getEventListHTML() {
+function renderEventListHTML() {
 	return `
 		<div class = "items">
 			<ul>
-				<li>Item</li>
+				<li class="title">${response.name.text}</li>
 			</ul>
 		</div>
 	`;
@@ -46,7 +59,20 @@ function renderEventList(html) {
 
 }
 
-getDataFromEventbrite(30188);
+
+function watchSubmit() {
+	$('#js-search-form').submit(event => {
+		event.preventDefault();
+		const queryTarget = $(event.currentTarget).find('#js-query');
+		const query = queryTarget.val();
+		console.log(query);
+		queryTarget.val("");
+		getDataFromEventbrite(query);
+
+	});
+	
+	console.log('watchSubmit ran');
+}
 
   var map;
       function initMap() {
@@ -59,4 +85,4 @@ getDataFromEventbrite(30188);
   console.log("initMap ran");
 }
 
-
+$(watchSubmit);
